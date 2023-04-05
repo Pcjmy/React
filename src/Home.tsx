dd6984df6701c8b7121aa7a5dc6effcf53615791
Pcjmy <1716045191@qq.com>
2023-04-05 21:07:11 +0800

    feat: createPortal

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import styles from './Home.module.css'
+import { createPortal } from 'react-dom'
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
   return (
-    <div className={styles.App}>
-      <header className={styles['App-Header']}>
-        <button>导入JSON</button>
-        <button>预览</button>
-        <button>撤销</button>
-        <button>重做</button>
-        <button>清空</button>
-      </header>
-      <div className={styles.content}>
-        <div className={styles['nav-layout']}>组件库</div>
-        <div className={styles['main-layout']}></div>
-        <div className={styles['aside-layout']}>
-          <div className={styles['item']}>属性</div>
-        </div>
-      </div>
+    <div style={{ border: '2px solid black' }}>
+      <p>This child is placed in the parent div.</p>
+      {createPortal(
+        <p>This child is placed in the document body.</p>,
+        document.body
+      )}
     </div>
   )
 }
