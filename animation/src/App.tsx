0537de2465b1b257222d8c2177ae350136a0aa15
Pcjmy <1716045191@qq.com>
2026-01-24 23:28:09 +0800

    feat: shared layout animation

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 // import Rotate from './components/rotate';
 // import EnterAnimation from './components/enter-animation'
-import Gestures from './components/gestures'
+// import Gestures from './components/gestures'
+import SharedLayoutAnimation from './components/shared-layout-animation'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <Gestures />
+      <SharedLayoutAnimation />
     </div>
   )
 }
