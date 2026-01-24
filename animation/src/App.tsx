54bfaa11eb5baf6391fae363d87034a50bc655de
Pcjmy <1716045191@qq.com>
2026-01-24 23:11:47 +0800

    feat: gestures

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 // import Rotate from './components/rotate';
-import EnterAnimation from './components/enter-animation'
+// import EnterAnimation from './components/enter-animation'
+import Gestures from './components/gestures'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <EnterAnimation />
+      <Gestures />
     </div>
   )
 }
