4d05f692ace4d773227047688b76acb9769032b0
Pcjmy <1716045191@qq.com>
2026-01-24 22:50:06 +0800

    feat: enter animation

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import Rotate from './components/rotate';
+// import Rotate from './components/rotate';
+import EnterAnimation from './components/enter-animation';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Rotate />
+      <EnterAnimation />
     </div>
   );
 }
