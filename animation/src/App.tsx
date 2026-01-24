176da77d833788ee4e9164dcb0d7d192e9a50fed
Pcjmy <1716045191@qq.com>
2026-01-24 21:58:46 +0800

    feat: motion react

---
+++ animation/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rotate from './components/rotate';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Rotate />
+    </div>
+  );
+}
+
+export default App;
